3e7feedd62b133b63ecf644b33249b24749fb366
cqrita <52904285+cqrita@users.noreply.github.com>
2024-07-11 20:10:53 +0900

    commit step 8

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedOut
+} from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +23,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{baseTheme: dark}}>
+      <html lang="en">
+        <SignedOut>
+            <RedirectToSignIn />
+        </SignedOut>
+        <body className={inter.className}>
+          <ThemeProvider 
+          attribute="class"
+          forcedTheme="dark"
+          storageKey="gamehub-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
